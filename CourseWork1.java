ce4677cce2190e4c85bf08b0d3878ce6d7d773d0
Fab Graminhani <fgrami01@dcs.bbk.ac.uk>
2014-10-04 02:02:51 +0100

    save n2

--- CourseWork1.java
+++ CourseWork1.java
@@ -1,35 +1,33 @@
-package coursework1;
+import java.lang.System;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Scanner; 
 
-
-/**
- * Created by Fabricio Graminhani on 30/09/2014.
- */
-public class CourseWork1 {
-    public static void main(String[] args) {
+public class Coursework1 {
+	public static void main(String[] args) {
 
         //creates array 1 to hold up to 100 elements
         final int LENGTH = 100;
         int[] array1 = new int[LENGTH];
         int[] array2 = new int[LENGTH];
+        int sentinel = 0;
+        int currentSize1 = 0;
+        int currentSize2 = 0;
 
-        int currentSizeArr1 = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please insert data for array 1: ");
+        System.out.print("Please insert data for array 1: ");
         while (input.hasNextInt()) {
-            for (int i = 0; i < LENGTH; i++) {
+            for(int i = 0; i < LENGTH; i++){
+            array1[i] = input.nextInt();
 
-                array1[i] = input.nextInt();
-
-                System.out.print(array1[i]);
-                //System.out.println("Array1 elements are: " + Arrays.toString(array1));
-                //for (int j = 0; j < array1.length; j++) {
-                //System.out.println(array1[j]);
-                //}
+            System.out.print("A porra eh essa: " + array1[i] + ",");
             }
+
+            //System.out.print(" ");
+
+
         }
+       
     }
 }
 
