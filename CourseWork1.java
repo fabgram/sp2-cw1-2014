0b5e5c9fb911c8657a8507cd6f5b9b80dbdf7090
Fab Graminhani <fgrami01@dcs.bbk.ac.uk>
2014-10-05 18:54:49 +0100

    Number of common data added

--- CourseWork1.java
+++ CourseWork1.java
@@ -1,66 +1,71 @@
-import java.lang.System;
 import java.util.Arrays;
-import java.util.Scanner; 
+import java.util.Scanner;
+/*
+ * Software and Programming II
+ * Fabricio Graminhani
+ * username: fgrami01
+ * date:05/10/2014
+ */
 
 public class Coursework1 {
 	public static void main(String[] args) {
 
-        //creates array 1 to hold up to 100 elements
         final int LENGTH = 100;
+        //creates arrays 1 and 2 to hold up to 100 elements
         int[] array1 = new int[LENGTH];
         int[] array2 = new int[LENGTH];
+        int flag = -1;
+        int counter = 0;
         Scanner input = new Scanner(System.in);
+
+        for(int i = 0; i < array1.length; i++){
+        	System.out.print("Please insert data for array 1 (0 to quit): ");
+    		flag = input.nextInt();
+        	if(flag == 0){
+        		break;	
+        	}
+        	array1[i] = flag;
+        }
+          
+        for(int i = 0; i < array2.length; i++){
+        	System.out.print("Please insert data for array 2 (0 to quit): ");
+    		flag  = input.nextInt();;
+        	if(flag == 0){
+        		break;	
+        	}
+        	array2[i] = flag;
+        }
         
+        System.out.print("\nValues for array 1 is: ");
+        for(int i = 0; i < array1.length; i++){
+        	if(array1[i] != 0){
+        		System.out.print(array1[i] + " ");
+        	}
+        }
+        
+        System.out.print("\nValues for array 2 is: ");
+        for(int i = 0; i < array2.length; i++){
+        	if(array2[i] != 0){
+        		System.out.print(array2[i] + " ");
+        	}
+        }
 
-            for(int i = 0; i < array1.length; i++){
-            	System.out.print("Please insert data for array 1 (0 to quit): ");
-            	array1[i]  = input.nextInt();
-            	if(array1[i] == 0){
-            		break;	
-            	}
-            }
-            
-            for(int i = 0; i < array2.length; i++){
-            	System.out.print("Please insert data for array 2 (0 to quit): ");
-            	array2[i]  = input.nextInt();
-            	if(array2[i] == 0){
-            		break;	
-            	}	
-            }
-            	
-       
-            
-            System.out.print("\nValues for array 1 is: ");
-            for(int i = 0; i < array1.length; i++){
-            	if(array1[i] != 0){
-            		System.out.print(array1[i] + " ");
-            	}	
-            }
-            
-            System.out.print("\nValues for array 2 is: ");
-            for(int i = 0; i < array2.length; i++){
-            	if(array2[i] != 0){
-            		System.out.print(array2[i] + " ");
-            	}
-            }
-            
-            //int[] common = new int[100];
-            System.out.print("\nCommon data is: ");
-            for(int i = 0; i < array1.length; i++){
-            	for(int j = 0; j < array2.length; j++){
-            		if(array1[i] == array2[j]){
-            			 array1[i] = array1[i];
-            			 if(array1[i] != 0){
-                     		System.out.print(array1[i] + " ");
-                     	}
-            		}
-            	}
-            }	
-            
-		}  
-                
+        System.out.print("\nCommon data is: ");
+        for(int i = 0; i < array1.length; i++){
+        	for(int j = 0; j < array2.length; j++){
+        		if(array1[i] == array2[j]){			 
+    			  if(array1[i] != 0){
+             	  System.out.print(array1[i] + " ");
+             	  counter++;
+    			  } 
+        		}
+        	}
+        }
+        System.out.print("\nNumber of common data: " + counter);
+        input.close();
 	}
-
+	
+}
 
        
     
